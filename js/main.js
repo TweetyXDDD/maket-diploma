272b984e540e3bf74a8490320c2420a503b4ae85
Eugene <eugene.polishchuk@gmail.com>
2019-10-13 22:46:51 +0300

    more new

--- js/main.js
+++ js/main.js
@@ -48,13 +48,42 @@ $(function() {
         curThIndex = null;
 
     $(function(){
-        $('table thead tr th').click(function(){
+        $('.table-one thead tr th').click(function(){
             thIndex = $(this).index();
             if(thIndex != curThIndex){
                 curThIndex = thIndex;
                 sorting = [];
                 tbodyHtml = null;
-                $('table tbody tr').each(function(){
+                $('.table-one tbody tr').each(function(){
+                    sorting.push($(this).children('td').eq(curThIndex).html() + ', ' + $(this).index());
+                });
+
+                sorting = sorting.sort();
+                sortItOne();
+            }
+        });
+    })
+
+    function sortItOne(){
+        for(var sortingIndex = 0; sortingIndex < sorting.length; sortingIndex++){
+            rowId = parseInt(sorting[sortingIndex].split(', ')[1]);
+            tbodyHtml = tbodyHtml + $('.table-one tbody tr').eq(rowId)[0].outerHTML;
+        }
+        $('.table-one tbody').html(tbodyHtml);
+    }
+});
+$(function() {
+    var thIndex = 0,
+        curThIndex = null;
+
+    $(function(){
+        $('.table-sec>thead>tr>th').click(function(){
+            thIndex = $(this).index();
+            if(thIndex != curThIndex){
+                curThIndex = thIndex;
+                sorting = [];
+                tbodyHtml = null;
+                $('table.table-sec>tbody>tr').each(function(){
                     sorting.push($(this).children('td').eq(curThIndex).html() + ', ' + $(this).index());
                 });
 
@@ -67,8 +96,8 @@ $(function() {
     function sortIt(){
         for(var sortingIndex = 0; sortingIndex < sorting.length; sortingIndex++){
             rowId = parseInt(sorting[sortingIndex].split(', ')[1]);
-            tbodyHtml = tbodyHtml + $('table tbody tr').eq(rowId)[0].outerHTML;
+            tbodyHtml = tbodyHtml + $('.table-sec>tbody>tr').eq(rowId)[0].outerHTML;
         }
-        $('table tbody').html(tbodyHtml);
+        $('.table-sec>tbody').html(tbodyHtml);
     }
 });
